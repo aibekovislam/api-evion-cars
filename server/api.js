f6cfcb4c09fb59ae87a955b662b6c219e4249dfd
Islam Aibekov <islamaibekov@Islams-MacBook-Air.local>
2023-09-08 16:32:30 +0600

    added image endpoint

--- server/api.js
+++ server/api.js
@@ -51,6 +51,28 @@ app.get('/getProducts', async (req, res) => {
   }
 });
 
+app.get('/getImages/:id', async (req, res) => {
+  try {
+    const response = await axios.get(`https://online.moysklad.ru/api/remap/1.2/entity/product/${req.params.id}/images`, {
+      headers: {
+        Authorization: 'Bearer 224525b73a9c5cf1f31973127b90897be84d1916',
+      }
+    })
+    if (response.status === 200) {
+      const data = response.data;
+      res.json(data);
+    } else {
+      res.status(response.status).json({
+        message: 'Error fetching data from the external API',
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'Internal server error',
+    });
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
