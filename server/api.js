c11429da507a58bba6e194221f1aaf5b395e6123
Islam Aibekov <islamaibekov@Islams-MacBook-Air.local>
2023-09-08 11:47:11 +0600

    server and delpoy api

---
+++ server/api.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization'
+  );
+  next();
+});
+
+
+app.get('/getProducts', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://online.moysklad.ru/api/remap/1.2/entity/product',
+      {
+        headers: {
+          Authorization: 'Bearer 224525b73a9c5cf1f31973127b90897be84d1916',
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      const data = response.data;
+      const filteredDataPromises = data.rows.map(async (item) => {
+        if (item.pathName === "Каталог товаров/Электромобили") {
+          return item;
+        }
+      });
+
+      const filteredData = (await Promise.all(filteredDataPromises)).filter(Boolean);
+      res.json(filteredData);
+    } else {
+      res.status(response.status).json({
+        message: 'Error fetching data from the external API',
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'Internal server error',
+    });
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
